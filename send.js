4daf23fae14d43dba29093f4f432060c8e36e4f2
awinecki <winecki@gmail.com>
2014-12-20 01:13:04 +0100

    Add chalk, clean up code, add error handling

--- send.js
+++ send.js
@@ -1,40 +1,51 @@
 #!/bin/sh
 ':' //; exec "$(command -v nodejs || command -v node)" "$0" "$@"
 
-var knox = require('knox');
-var Slack = require('node-slack');
-var git = require('git-rev');
-var fs = require('fs');
 var argv = require('minimist')(process.argv.slice(2));
+var chalk = require('chalk');
+var fs = require('fs');
+var git = require('git-rev');
+var knox = require('knox');
+var slack = require('node-slack');
+
 
 var S3_ACCESS_KEY_ID = 'AKIAIUG6LZCOC62UEA3Q';
 var S3_SECRET_ACCESS_KEY = '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';
+var S3_BUCKET_NAME = 'haicommits';
+var SLACK_TEAM_NAME = 'haikode';
+var SLACK_TOKEN = 'ThbSB26xsZrwk7NETJNhXla3';
+var SLACK_DEFAULT_CHANNEL = '#test444';
+var user = process.env.USER;
+var repoDirname = process.cwd().split('/').pop();
 
+var slackClient = new slack(SLACK_TEAM_NAME, SLACK_TOKEN);
 var s3client = knox.createClient({
   key: S3_ACCESS_KEY_ID,
   secret: S3_SECRET_ACCESS_KEY,
-  bucket: 'haicommits'
+  bucket: S3_BUCKET_NAME
 });
 
-var slack = new Slack('haikode', 'ThbSB26xsZrwk7NETJNhXla3');
-var repoDirname = process.cwd().split('/').pop();
-var user = process.env.USER;
-
-
 function bucketPath(filePath) {
-  return 'https://s3-eu-west-1.amazonaws.com/haicommits/' +
+  return 'https://s3-eu-west-1.amazonaws.com/' + S3_BUCKET_NAME + '/' +
     filePath;
 }
 
 function sendSlackMsg(msg) {
-  slack.send({
+  slackClient.send({
       text: msg,
-      channel: argv.channel || '#test444'
+      channel: argv.channel || SLACK_DEFAULT_CHANNEL
   });
 }
 
 function saveLocalPictureToAmazon(localPath) {
   fs.stat(localPath, function(err, stat){
+    if (!stat) {
+      console.log(chalk.yellow(
+        'Lolcommit img/gif not found for the last commit.'),
+        chalk.gray('Tried ' + localPath)
+      );
+      return;
+    }
     var filename = localPath.split('/').pop();
     var s3path = user + '/' + repoDirname + '/' + filename;
     var req = s3client.put(s3path, {
@@ -48,7 +59,6 @@ function saveLocalPictureToAmazon(localPath) {
   });
 }
 
-
 // run
 git.long(function (str) {
   var lastCommitSha = str.substring(0, 11);
@@ -56,4 +66,4 @@ git.long(function (str) {
   var commitPicturePath = '/Users/' + user + '/.lolcommits/' +
     repoDirname + '/' + lastCommitSha + ext;
   saveLocalPictureToAmazon(commitPicturePath);
-})
+});
